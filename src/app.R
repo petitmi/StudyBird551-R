a476c0b437e2bcf5f2f0e69ce84f29d90537eb43
chen-yuhong <59632334+chen-yuhong@users.noreply.github.com>
2023-03-09 20:51:59 -0800

    Update app.R
    add lyrics page content

--- src/app.R
+++ src/app.R
@@ -4,10 +4,14 @@ library(dashHtmlComponents)
 library(dashBootstrapComponents)
 library(ggplot2)
 library(plotly)
-library(tidyverse)
+library(readxl)
 library(dplyr)
-library(RColorBrewer)
-library(wordcloud)
+library(tidyr)
+library(tidyverse)
+library(wordcloud2)
+library(webshot)
+library(htmlwidgets)
+library(base64enc)
 
 app <- Dash$new(external_stylesheets = dbcThemes$QUARTZ)
 years <- c()
@@ -108,10 +112,85 @@ generate_page_content <- function(page_title, year) {
         
     } else if (page_title=="Lyrics Analysis"){
         
-        df <- data.frame(x = rnorm(100), y = rnorm(100))
-        plot1<-ggplot(data = df, aes(x = x, y = y)) + geom_point()
-        plot2<-ggplot(data = df, aes(x = x, y = y)) + geom_point()
-        plot3<-ggplot(data = df, aes(x = x, y = y)) + geom_point()
+        options(dplyr.summarise.inform = FALSE)
+        df <- read_excel('./data/processed/lyrics_dataset.xlsx')
+        df <- df %>% filter(Year >= start_year & Year <= end_year)
+        cutRank4<-cut(df$Rank, breaks = 4,labels=c('1-25','26-50','51-75','76-100'))
+        df$rank_bin <-cutRank4
+
+        df_bin <- df %>% 
+        select(c('rank_bin','Year','Word_Count','Sentiment_Polarity','Frequency_love'))%>% group_by(rank_bin,Year)%>%
+        summarise(across(c('Word_Count', 'Sentiment_Polarity','Frequency_love'), mean))%>% as.data.frame()
+        source <- df %>% count(Sentiment)%>% as.data.frame()
+        cutRank10<-cut(df$Rank, breaks = 10,labels=c('1-10','11-20','21-30','31-40','41-50','51-60','61-70','71-80','81-90','91-100'))
+        df$rank_bin10 <-cutRank10
+        df_bin10 <- df %>% 
+        select(c('rank_bin10','Year','Word_Count','Sentiment_Polarity','Frequency_love'))%>% group_by(rank_bin10,Year)%>%
+        summarise(across(c('Word_Count', 'Sentiment_Polarity','Frequency_love'), mean))%>% as.data.frame()
+        if(toString(start_year)==toString(end_year)){
+            plot1<-ggplot(df_bin10, aes(x=Year, y=Frequency_love, fill=rank_bin10)) +
+              geom_bar(stat="identity", position="stack") +
+              scale_x_discrete(name="Year") +
+              scale_y_continuous(name="Frequency") +
+              ggtitle(sprintf("Frequency of 'love' Analysis")) +
+              theme_minimal() +
+              theme(plot.title = element_text(hjust = 0.5), 
+              legend.position="bottom", 
+              legend.title=element_blank(), 
+              legend.direction = "horizontal")
+
+        }else{
+            plot1<-ggplot(df_bin10, aes(x = Year, y = Frequency_love, fill = factor(rank_bin10))) + 
+        geom_area(stat = "identity", alpha = 0.8) + 
+        scale_fill_discrete(name = "rank_bin10") +
+        labs(title = sprintf("Frequency of 'love' Analysis"), x = "Year", y = "Frequency") + 
+        theme(plot.title = element_text(hjust = 0.5))
+        }
+        
+        
+        
+        plot2<-ggplot(df_bin10, aes(x=factor(Year), y=Word_Count, fill=rank_bin10)) +
+          geom_bar(stat="identity", position="stack") +facet_grid(~rank_bin10)+
+          scale_x_discrete(name="Year") +
+          scale_y_continuous(name="Lyrics Length") +
+          ggtitle(sprintf("Lyrics Length Anaylisis")) +
+          theme_minimal() +
+          theme(plot.title = element_text(hjust = 0.5), 
+                axis.text.x = element_text(size=5,angle = 90, vjust = 0.5, hjust=1),
+                legend.position="right", 
+                legend.title=element_blank(), 
+                legend.direction = "horizontal")
+        
+        
+        p1<-ggplot(source, aes(x=Sentiment, y=n, fill=Sentiment))+
+            geom_bar(width = 1, stat = "identity",color="white")
+        
+        p2<-ggplot(df, aes(x=factor(Year), y=Sentiment_Polarity, fill=factor(Year))) +
+            geom_boxplot()+
+            scale_x_discrete(name="Year") +
+            scale_y_continuous(name="Sentiment Score")+
+            ggtitle(paste('Boxplot of Sentiment Score Analysis'))
+
+        plot3<-subplot(p1, p2, nrows = 1)%>%layout(legend=list(title=list(text='')))
+
+
+        df$'word_cloud'<- df$'Clean_Lyrics'
+        remove_words <- c('verse', 'chorus', 'oh', 'want', 'yeah', 'wan', 'na', 'got', 'might', 'feat')
+        for (word in remove_words) {
+              df$'word_cloud'<- gsub(paste0("\\b", word, "\\b"), '', df$'word_cloud', perl=TRUE)}
+        text <- paste(df$'word_cloud', collapse = " ")
+        my_words <- strsplit(tolower(text), "\\W+")[[1]]
+        word_freq <- table(my_words)
+        word_df <- as.data.frame(word_freq, stringsAsFactors = FALSE)
+        names(word_df) <- c("word", "frequency")
+        word_df <- word_df[order(-word_df$frequency),]
+        pic<-wordcloud2(word_df, size = 1, color = "random-dark")
+        saveWidget(pic, "tmp.html", selfcontained = F)
+        webshot("tmp.html", "wordcloud.png", delay = 3, vwidth = 900, vheight = 200)
+        img <- readBin("wordcloud.png", "raw", file.info("wordcloud.png")$size)
+        img_base64 <- base64encode(img)
+        md_code <- paste0("![](data:image/png;base64,", img_base64, ")")
+
         
         chart_describ=dccMarkdown("
         - Divide the top 100 songs into 10 groups; 
@@ -127,12 +206,9 @@ generate_page_content <- function(page_title, year) {
         - From the figure, From the figure, we can compare each year's lyrics length in the four rank groups.
         ")
         
-        chart2_describ=dccMarkdown("
-            - Chart on the left is a pie chart representing the proportion of positive, negative and neutral lyrics in the selected year. 
-            - Placing the mouse on the pie chart can show the specific number of songs; 
-            - The chart on the right is the boxplot which displays ranges within sentiment scores for the selected year; 
-            - sentiment score ranges from -1 to 1. (-1 is the most negative, 1 is the most positive, and 0 is neutral).
-            ")
+        chart2_describ=dccMarkdown(list("- Chart on the left is a bar chart displaying the number of positive, negative and neutral lyrics in the selected year.", 
+            "- The chart on the right is the boxplot which displays ranges within sentiment scores for the selected year;", 
+            "- Sentiment score ranges from -1 to 1. (-1 is the most negative, 1 is the most positive, and 0 is neutral).",md_code))
         
     } else if (page_title=="Tracks Analysis"){
         
